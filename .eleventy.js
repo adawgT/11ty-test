6d03a0cfcff7d6700576f814a8a577e9d8ec93cb
adawgT <nguy1434@msu.edu>
2025-02-03 02:17:30 +0000

    edited files and added blog posts

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,15 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addPassthroughCopy("**/*.jpg");
+  eleventyConfig.addShortcode("formatDate", function(date) {
+    return new Date(date).toDateString();
+  });
+
+  return {
+    dir: {
+      input: "site",
+      output: "_site"
+    }
+  };
 };
---
+++ index.md
@@ -0,0 +1,12 @@
+---
+title: "Interstellar Explorer's Log"
+layout: "layouts/base.njk"
+---
+
+# Welcome to the Interstellar Explorer's Log
+
+A lone traveler among the stars, recording discoveries across the cosmos.
+
+## Recent Entries
+- [Echoes in the Void](/blog/entry1/)
+- [The Ghost Planet](/blog/entry2/)
